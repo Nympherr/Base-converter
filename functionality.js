9f288819231c874659f926b6b47558c70653e0c8
Lukas S <114870613+Nympherr@users.noreply.github.com>
2023-03-17 17:15:59 +0200

    Add conversion with integer numbers
    Program correctly converts integer numbers
    from one base to another. It does so by
    firstly converting a number to base 10
    and with quotient and remainder method
    it converts to whatever base system user
    wants

--- functionality.js
+++ functionality.js
@@ -14,10 +14,47 @@ reset.addEventListener("click", function(){
 
 convert.addEventListener("click",function() {
     let input_value = parseFloat(input.value);
+    let sum;
+    let finalResult;
+
     if(!isNaN(input_value)){
-        output.value = input_value;
+        sum = base10Integer(input_value,base_from.value);
+        finalResult = otherSystemInteger(sum, base_to.value);
+        output.value = finalResult;
     }
     else{
         output.value = "Only numbers are allowed!"
     }
 });
+
+function base10Integer(number,base_from){
+
+    let base_value = parseFloat(base_from);
+    let number_array = number.toString().split("").reverse().map(Number);
+    let sum = 0;
+    for(let i = 0; i < number_array.length;i++){
+        sum += number_array[i] * Math.pow(base_value, i);
+    }
+    return sum;
+};
+
+function otherSystemInteger(number,base_to){
+    let quotient = parseInt(number);
+    let remainder;
+    let baseToValue = parseInt(base_to);
+    let result = "";
+
+    do {
+    remainder = quotient % baseToValue;
+    result += remainder.toString();
+    quotient = Math.floor(quotient / baseToValue);
+    } while (quotient != 0);
+
+    let finalResult = result.split("").reverse().join("");
+    finalResult = parseInt(finalResult);
+    return finalResult;
+}
+
+function integerConvert(){
+    console.log("hey");
+}
